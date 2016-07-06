0f0d5744aeaf76569c87923fa3ac17a38f2b39ee
balag3 <carbalage@gmail.com>
2016-07-06 11:22:59 +0200

    creates Parent class of all tasks

--- task.py
+++ task.py
@@ -1,6 +1,6 @@
 class Task(object):
 
-    def __init__(name,priority,color_mix):
+    def __init__(self,name,priority,color_mix):
         self.name = name
         self.priority = priority
         self.color_mix = color_mix
