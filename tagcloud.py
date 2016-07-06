a18cd78aa66ca2228afc8af5e1f01379a95994d4
Adam <horada1991@gmail.com>
2016-07-06 17:30:42 +0200

    Colors added to draw

--- tagcloud.py
+++ tagcloud.py
@@ -1,12 +1,13 @@
 from client import Client
+from company_name import Company
 from project import Project
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageColor
 import random
 
 
 class TagCloud():
 
-    def __init__(self, object_list, width=600, height=600, background_color="#fff", font="arial_narrow_7.ttf"):
+    def __init__(self, object_list, width=700, height=700, background_color="#fff", font="Hugs & Kisses xoxo Demo.ttf"):
         self.width = width
         self.height = height
         self.image = Image.new('RGBA', [width, height], background_color)
@@ -19,21 +20,21 @@ class TagCloud():
         self._word_list()
         for word in self.words:
             self.draw.text((word['position'][0], word['position'][1]), word['text'],
-                           font=ImageFont.truetype(self.font, word['font_size']), fill=(0, 0, 0))
-        self.image.save('TagCloud.jpg')
+                           font=ImageFont.truetype(self.font, word['font_size']), fill=word['fill'])
         self.image.show()
 
     # Fill up words list with the necessary text properties
     def _word_list(self):
         for object in self.object_list:
             text = object.name
-            font_size = int(object.priority / 3) + 20
+            color = object.rgb_color
+            font_size = int(object.priority / 3)+10
             width, height = self.draw.textsize(text, font=ImageFont.truetype(self.font, font_size))
             position = [random.randint(0, self.width - width), random.randint(0, self.height - height)]
             while not self._overlap(position, width, height):
                 position = [random.randint(0, self.width - width), random.randint(0, self.height - height)]
             self.words.append({'text': text, 'font_size': font_size, 'width': width, 'height': height,
-                               'position': position})
+                               'position': position, 'fill': color})
 
     def _overlap(self, position, width, height):
         if len(self.words) > 0:
@@ -46,3 +47,10 @@ class TagCloud():
             return True
         else:
             return True
+
+
+
+#
+#
+# asd = TagCloud(Project.gen_list())
+# asd.drawing()
