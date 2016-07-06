6eaa93054e64990ef2e06a99c6b6bc6f8ad6274d
balag3 <carbalage@gmail.com>
2016-07-06 14:59:07 +0200

    Adds rgb color property.

--- task.py
+++ task.py
@@ -1,6 +1,12 @@
+from PIL import ImageColor
+
 class Task(object):
 
     def __init__(self,name,priority,color_mix):
         self.name = name
         self.priority = priority
         self.color_mix = color_mix
+
+    @property
+    def rgb_color(self):
+        return ImageColor.getrgb(self.color_mix)
