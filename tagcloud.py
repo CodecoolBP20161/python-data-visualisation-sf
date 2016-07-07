94ddf0d851c77a2fbb39ae5bd2b0a51c8006b411
Adam <horada1991@gmail.com>
2016-07-07 15:41:01 +0200

    tagcloud.py update, and some fixes

--- tagcloud.py
+++ tagcloud.py
@@ -13,7 +13,8 @@ class TagCloud():
     FONTS = {'aquawax': 'Aquawax Black Trial.ttf', 'arial': 'arial_narrow_7.ttf', 'DK': 'DK Cinnabar Brush.ttf',
              'keepcalm': 'KeepCalm-Medium.ttf', 'kenyan': 'kenyan coffee bd.ttf'}
     MAIN_PATH = os.path.dirname(os.path.abspath(__file__))
-    def __init__(self, object_list, width=1000, height=800, background_color="#fff", font="DK Cinnabar Brush.ttf"):
+
+    def __init__(self, object_list, width=1000, height=700, background_color="#fff", font="DK Cinnabar Brush.ttf"):
         self.width = width
         self.height = height
         self.image = Image.new('RGBA', [width, height], background_color)
@@ -23,25 +24,34 @@ class TagCloud():
         self.object_list = object_list
         self._check_window_occupation = False
 
-    def drawing(self):
+    #draw out and save the tagcloud
+    def drawing(self, file_name='tagcloud.png'):
         self._word_list()
         for word in self.words:
             self.draw.text((word['position'][0], word['position'][1]), word['text'],
                            font=ImageFont.truetype(self.font, word['font_size']), fill=word['fill'])
+        self._crop_blank()
+        logo = Image.open("starfish.png")
+        logo.thumbnail((50, 50))
+        self.image.paste(logo, (self.width - 50, 0))
         self.image.show()
-        self.image.save('blabla.png')
+        self.image.save(file_name)
+
 
     # Fill up words list with the necessary text properties
     def _word_list(self, font_size_calculator=5):
 
+        # fills up words attribute
         for object in self.object_list:
             text = object.name
 
+            # get color of the text
             try:
                 color = object.mixer()
             except AttributeError:
                 color = object.rgb_color
 
+            # give the text a font_size
             if object.priority > 90:
                 font_size = int(object.priority / font_size_calculator)
             elif object.priority > 85:
@@ -50,9 +60,10 @@ class TagCloud():
                 font_size = int(object.priority / (font_size_calculator * 2))
 
 
-
+            # get width and height of the text
             width, height = self.draw.textsize(text, font=ImageFont.truetype(self.font, font_size))
 
+            # give position for the text
             position = [self.width/2 - width, self.height/2 - height]
             t = random.randint(1, 100)
             spiral_coord = (0.25 * (math.cos(t) + t * math.sin(t)), 0.25 * (math.sin(t) - t * math.cos(t)))
@@ -90,12 +101,14 @@ class TagCloud():
         else:
             return True
 
+    # check if the window too large
     def _is_window_large(self):
         x_max, x_min, y_max, y_min = self._check_tag_borders()
         if x_max < self.width and x_min > 0 and y_max < self.height and y_min > 0:
             return True
         return False
 
+    # check the tag borders
     def _check_tag_borders(self):
         x_max, x_min, y_max, y_min = 0, self.width, 0, self.height
         for word in self.words:
@@ -113,5 +126,8 @@ class TagCloud():
                 y_min = text_y_min
         return x_max, x_min, y_max, y_min
 
-inst = TagCloud(Headquarter.gen_list(), font='arial_narrow_7.ttf')
-inst.drawing()
+    def _crop_blank(self):
+        x_max, x_min, y_max, y_min = self._check_tag_borders()
+        box = (int(x_min), int(y_min), int(x_max), int(y_max))
+        self.image = self.image.crop(box)
+        self.width, self.height = self.image.size
